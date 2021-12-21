9d25e6b358c3f5970d2d402cb6df5a2109a08024
Leslie Salomon <useripad2@email.com>
2021-12-21 02:25:12 -0500

    Feature  , agregando una funcion suma

--- src/app.py
+++ src/app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "hola mundo"
-
+@app.route('/sum/<int:a>/<int:b>')
+def sum(a: int, b: int):
+    suma = a+b
+    return f"La suma es: {str(suma)}"
 #app * Flask(__name__)
