4b4d60e1a27f242cf07f702f4bb4f72336b6f04f
Leslie Salomon <useripad2@email.com>
2021-12-21 02:08:48 -0500

    Feature,agregando saludo por medio de una ruta

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
 #def sum (a,b):
 #    return a+b
-from flask import flask
+from flask import Flask
 
-app * Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hola mundo"
+
+#app * Flask(__name__)
